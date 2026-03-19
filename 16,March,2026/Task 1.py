28450f0113ef96e4526efb508ad6b838b7f73a59
Harsh Didwania <harshagarwal132003@gmail.com>
2026-03-19 08:46:30 +0530

    Update Task 1.py

--- 16,March,2026/Task 1.py
+++ 16,March,2026/Task 1.py
@@ -4,38 +4,19 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 
 driver = webdriver.Chrome()
-
-
 driver.get("https://opensource-demo.orangehrmlive.com/")
 sleep(3)
-
-
 print("Page Title:", driver.title)
-
 username = driver.find_element(By.NAME, "username")
 username.clear()
-
-
 username.send_keys("Admin")
-
-
 password = driver.find_element(By.NAME, "password")
 password.send_keys("admin123")
-
-
 login_btn = driver.find_element(By.XPATH, "//button[@type='submit']")
 login_btn.click()
-
 sleep(4)
-
-
 current_url = driver.current_url
 print("Current URL:", current_url)
-
-
 if "dashboard" in current_url.lower():
-
     print("Successful login")
-
 sleep(3)
-driver.quit()
