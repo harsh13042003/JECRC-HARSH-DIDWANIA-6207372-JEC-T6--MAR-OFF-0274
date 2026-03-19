cea5a3f331896f0e44f23d12e7156022a41e51dd
Harsh Didwania <harshagarwal132003@gmail.com>
2026-03-19 21:53:00 +0530

    Update Task1.py

--- 19March)/Task1.py
+++ 19March)/Task1.py
@@ -3,6 +3,7 @@ from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 driver = webdriver.Chrome()
 driver.get("https://abc.com/")
 driver.maximize_window()
