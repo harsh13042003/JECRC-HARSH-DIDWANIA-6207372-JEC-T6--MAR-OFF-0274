837c03fb2a44d621eb8eb9ed6d1b9c9ee14c675f
Harsh Didwania <harshagarwal132003@gmail.com>
2026-03-23 23:23:47 +0530

    Task2.py

--- 20,March,2026/Task2.py
+++ 20,March,2026/Task2.py
@@ -9,13 +9,8 @@ driver.get(r"C:\Users\Pranjal Ameta\OneDrive\Desktop\Selenium&Robot\Projects\Day
 drop_sel=driver.find_element(By.ID,'songs')
 
 select=Select(drop_sel)
-
 fav_artist='Tame Impala'#input('enter favourite artist:')
-
 options_el=driver.find_elements(By.XPATH,f"//select[@id='songs']/optgroup[@label='{fav_artist}']/option")
-#print(len(options_el))
-# l=options_el.text.split("\n")
-
 for opt in options_el:
      select.select_by_visible_text(opt.text)
 sleep(4)
