c0a83012498a944678a0096050855b78f16f427d
Harsh Didwania <harshagarwal132003@gmail.com>
2026-03-23 23:25:52 +0530

    Task2.py

--- 23,March,2026/Task2.py
+++ 23,March,2026/Task2.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
 opts=webdriver.ChromeOptions()
 opts.add_experimental_option('detach',True)
 driver=webdriver.Chrome(options=opts)
@@ -14,7 +13,6 @@ driver.maximize_window()
 wait=WebDriverWait(driver,10)
 
 men_button=wait.until(EC.presence_of_element_located((By.XPATH,"//a[text()='Men']")))
-
 action=ActionChains(driver)
 action.move_to_element(men_button).perform()
 
@@ -24,4 +22,3 @@ button_sel.click()
 scroll_el=wait.until(EC.presence_of_element_located((By.XPATH,"//li[@id='37075364']")))
 action.scroll_to_element(scroll_el).perform()
 sleep(3)
-
