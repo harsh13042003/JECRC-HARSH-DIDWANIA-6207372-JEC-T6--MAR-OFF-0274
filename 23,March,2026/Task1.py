a63fe39b61b79a3207c75bbd6f9f75e1dea08d5c
Harsh Didwania <harshagarwal132003@gmail.com>
2026-03-23 23:25:15 +0530

    Task1.py

--- 23,March,2026/Task1.py
+++ 23,March,2026/Task1.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-
 driver = webdriver.Chrome()
 driver.maximize_window()
 action = ActionChains(driver)
@@ -14,9 +13,7 @@ pirelli = driver.find_element(By.XPATH, "//h2[text()='2026 HIGHLIGHTS']")
 
 action.scroll_to_element(pirelli).perform()
 sleep(2)
-
 for i in range(0,5):
     action.send_keys(Keys.PAGE_UP).perform()
     sleep(3)
-
 sleep(2)
